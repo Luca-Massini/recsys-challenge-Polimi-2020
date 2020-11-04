b8d98f46a0868c0e4da535dd25735f79ed56ba7e
luca-massini <37840272+Lucagibson96@users.noreply.github.com>
2020-11-04 23:57:16 +0100

    data manager and splitter implemented. Code still not commented

--- utils/data_manager/data_manager.py
+++ utils/data_manager/data_manager.py
@@ -3,85 +3,87 @@ import scipy.sparse as sparse
 import pandas as pd
 
 
-# This method casts any array in an integer numpy array
-def int_cast(array):
-    return np.array(list(map(int, array)))
-
-
-# This method takes in input some matrices with the same number of columns but possibly a different number of rows. It
-# resize all the matrices in such a way that in the end they will have the same number of rows
-def equalize_rows(matrices):
-    row = np.max([matrix.shape[0] for matrix in matrices])
-    for matrix in matrices:
-        matrix.resize((row, matrix.shape[1]))
-
-
+# This class builds all the matrices and imports also all the files from the data folder in the right way.
 class data_manager:
-    # A lot of the parameters of this class are private and this is clearly a choice done on purpose in order to hide
-    # implementation details not really necessary for using the class in a proper and efficient way. The use of this class
-    # in this way it's simpler.
-    def __init__(self):
-        # The names of the files to be downloaded. Until now they're empty strings since the competition is not opened
-        # yet. Both file_names array and names array will be static. The same holds for the indexes
-        self.__file_names = ["data_ICM_asset.csv", "data_ICM_price.csv", "data_ICM_sub_class.csv", "data_UCM_age.csv",
-                             "data_UCM_region.csv", "data_train.csv"]
-        self.__icm_files = [0, 1, 2]
-        self.__ucm_files = [3, 4]
-        self.__urm_files = [5]
-        self.__data_in_files = []
-        self.__download_files_offline()
-        self.__URM, self.__ICM, self.__UCM = self.__compute_matrices()
+    __instance = None
 
-    # This method returns a list of all the items identifiers
-    def get_items_list(self):
-        return self.__item_list
+    class __data_manager_instance:
+        def __init__(self):
+            # The names of the files to be downloaded. Until now they're empty strings since the competition is not opened
+            # yet. Both file_names array and names array will be static. The same holds for the indexes
+            self.__file_names = ["data_ICM_asset.csv", "data_ICM_price.csv", "data_ICM_sub_class.csv",
+                                 "data_UCM_age.csv",
+                                 "data_UCM_region.csv", "data_train.csv"]
+            self.__icm_files = [0, 1, 2]
+            self.__ucm_files = [3, 4]
+            self.__urm_files = [5]
+            self.__data_in_files = []
+            self.__download_files_offline()
+            self.__URM, self.__ICM, self.__UCM = self.__compute_matrices()
 
-    # This method returns a list of all the user identifiers
-    def get_user_list(self):
-        return self._user_list
+        # This method returns the URM matrix in csr matrix form
+        def get_urm(self):
+            return self.__URM
 
-    # This method returns the URM matrix in csr matrix form
+        # This method returns the UCM matrix in csr matrix form
+        def get_ucm(self):
+            return self.__UCM
+
+        # This method returns the ICM matrix in csr matrix form
+        def get_icm(self):
+            return self.__ICM
+
+        # This method imports the data from the csv files contained in the csv data folder
+        def __download_files_offline(self):
+            for file in self.__file_names:
+                file = "../../data/" + file
+                self.__data_in_files.append(pd.read_csv(file).to_numpy())
+
+        # This method casts any array into integer numpy array
+        @staticmethod
+        def __int_cast(array):
+            return np.array(list(map(int, array)))
+
+        # This method takes as input some matrices with the same number of columns but possibly a different number of rows. It
+        # resize all the matrices in such a way that in the end they will have the same number of rows
+        @staticmethod
+        def __equalize_rows(matrices):
+            row = np.max([matrix.shape[0] for matrix in matrices])
+            for matrix in matrices:
+                matrix.resize((row, matrix.shape[1]))
+
+        # It computes and builds the URM, ICM and UCM matrix (in sparse.csr_matrix format) and it gives them as result
+        def __compute_matrices(self):
+            files = [self.__urm_files, self.__ucm_files, self.__icm_files]
+            matrices = [[self.__data_in_files[i] for i in index] for index in files]
+            urm_matrices, ucm_matrices, icm_matrices = matrices[0], matrices[1], matrices[2]
+            urm_coo_matrices = [
+                sparse.coo_matrix((matrix[:, 2], (self.__int_cast(matrix[:, 0]), self.__int_cast(matrix[:, 1])))) for
+                matrix in urm_matrices]
+            icm_coo_matrices = [
+                sparse.coo_matrix((matrix[:, 2], (self.__int_cast(matrix[:, 0]), self.__int_cast(matrix[:, 1])))) for
+                matrix in icm_matrices]
+            ucm_coo_matrices = [
+                sparse.coo_matrix((matrix[:, 2], (self.__int_cast(matrix[:, 0]), self.__int_cast(matrix[:, 1])))) for
+                matrix in ucm_matrices]
+            self.__equalize_rows(urm_coo_matrices)
+            self.__equalize_rows(icm_coo_matrices)
+            self.__equalize_rows(ucm_coo_matrices)
+            URM = sparse.hstack(urm_coo_matrices, format='csr')
+            ICM = sparse.hstack(icm_coo_matrices, format='csr')
+            UCM = sparse.hstack(ucm_coo_matrices, format='csr')
+            return URM, ICM, UCM
+
+    def __new__(cls):
+        if not data_manager.__instance:
+            data_manager.__instance = data_manager.__data_manager_instance()
+        return data_manager.__instance
+
     def get_urm(self):
-        return self.__URM
+        return self.__instance.get_urm()
 
-    # This method returns the UCM matrix in csr matrix form
     def get_ucm(self):
-        return self.__UCM
+        return self.__instance.get_ucm()
 
-    # This method returns the ICM matrix in csr matrix form
     def get_icm(self):
-        return self.__ICM
-
-    # This method imports the data from the csv files contained in the csv data folder
-    def __download_files_offline(self):
-        for file in self.__file_names:
-            file = "../../data/" + file
-            self.__data_in_files.append(pd.read_csv(file).to_numpy())
-        self.__data_in_files = np.array(self.__data_in_files)
-
-    def __set_items_list(self, icm_matrices):
-        max_item_number = np.max([np.max(matrix[:, 0]) for matrix in icm_matrices])
-        self.__item_list = [int(i) for i in range(0, int(max_item_number + 1))]
-
-    def __set_user_list(self, ucm_matrices):
-        max_user_number = np.max([np.max(matrix[:, 0]) for matrix in ucm_matrices])
-        self._user_list = [int(i) for i in range(0, int(max_user_number + 1))]
-
-    # It computes and builds the URM, ICM and UCM matrix (in sparse.csr_matrix format) and it gives them as result
-    def __compute_matrices(self):
-        files = [self.__urm_files, self.__ucm_files, self.__icm_files]
-        matrices = [[self.__data_in_files[i] for i in index] for index in files]
-        urm_matrices, ucm_matrices, icm_matrices = matrices[0], matrices[1], matrices[2]
-        urm_coo_matrices = [sparse.coo_matrix((matrix[:, 2], (int_cast(matrix[:, 0]), int_cast(matrix[:, 1])))) for
-                            matrix in urm_matrices]
-        icm_coo_matrices = [sparse.coo_matrix((matrix[:, 2], (int_cast(matrix[:, 0]), int_cast(matrix[:, 1])))) for
-                            matrix in icm_matrices]
-        ucm_coo_matrices = [sparse.coo_matrix((matrix[:, 2], (int_cast(matrix[:, 0]), int_cast(matrix[:, 1])))) for
-                            matrix in ucm_matrices]
-        equalize_rows(urm_coo_matrices)
-        equalize_rows(icm_coo_matrices)
-        equalize_rows(ucm_coo_matrices)
-        URM = sparse.hstack(urm_coo_matrices, format='csr')
-        ICM = sparse.hstack(icm_coo_matrices, format='csr')
-        UCM = sparse.hstack(ucm_coo_matrices, format='csr')
-        return URM, ICM, UCM
+        return self.__instance.get_icm()
--- utils/data_manager/splitter.py
+++ utils/data_manager/splitter.py
@@ -1,9 +1,7 @@
 import numpy as np
 import scipy.sparse as sparse
 from utils.data_manager.data_manager import data_manager
-
-# so every time I will split the data, it will will be divided in the same way every single time
-np.random.RandomState = 1234
+from sklearn.model_selection import train_test_split
 
 
 class splitter:
@@ -19,19 +17,26 @@ class splitter:
                                       num_interactions,
                                       p=[percentage_of_training_data, 1 - percentage_of_training_data])
         test_mask = np.logical_not(train_mask)
-        URM_train = sparse.csr_matrix(
+        train_ = sparse.csr_matrix(
             (self.__URM.data[train_mask], (self.__URM.nonzero()[0][train_mask], self.__URM.indices[train_mask])))
-        URM_test = sparse.csr_matrix(
+        test_ = sparse.csr_matrix(
             (self.__URM.data[test_mask], (self.__URM.nonzero()[0][test_mask], self.__URM.indices[test_mask])))
-        return URM_train, URM_test
+        return train_, test_
 
-    def random_hold_out_ratings_validation(self, percentage_of_training_data, percentage_of_validation_data, percentage_of_testing_data=None):
-        assert (percentage_of_validation_data > 0 and percentage_of_training_data > 0 and (
-                    percentage_of_testing_data is None or percentage_of_testing_data > 0))
+    @staticmethod
+    def __assert_train_test_val_percentage(percentage_of_training_data, percentage_of_validation_data,
+                                           percentage_of_testing_data=None):
+        assert (percentage_of_validation_data > 0 and percentage_of_training_data > 0
+                and (percentage_of_testing_data is None or percentage_of_testing_data > 0))
         if percentage_of_testing_data is not None:
             assert (percentage_of_validation_data + percentage_of_training_data + percentage_of_testing_data == 1)
         else:
             assert (percentage_of_training_data + percentage_of_validation_data < 1)
+
+    def random_hold_out_ratings_validation(self, percentage_of_training_data, percentage_of_validation_data,
+                                           percentage_of_testing_data=None):
+        self.__assert_train_test_val_percentage(percentage_of_training_data, percentage_of_validation_data,
+                                                percentage_of_testing_data)
         tmp = percentage_of_training_data
         percentage_of_training_data = percentage_of_training_data + percentage_of_validation_data
         train_, test_ = self.random_hold_out_ratings_train_test(percentage_of_training_data)
@@ -48,5 +53,59 @@ class splitter:
             (tmp.data[validation_mask], (tmp.nonzero()[0][validation_mask], tmp.indices[validation_mask])))
         return train_, validation_, test_
 
-    def get_train_test_together(self):
+    @staticmethod
+    def __unzip_couples(couples1, couples2):
+        users_1 = [elem[0] for elem in couples1]
+        users_2 = [elem[0] for elem in couples2]
+        items_1 = [elem[1] for elem in couples1]
+        items_2 = [elem[1] for elem in couples2]
+        return users_1, users_2, items_1, items_2
+
+    def deterministic_hold_out_ratings_train_test(self, percentage_of_training_data=0.8):
+        assert (0 < percentage_of_training_data < 1)
+        users, items = self.__URM.nonzero()
+        n_users, n_items = np.max(users) + 1, np.max(items) + 1
+        couples = list(zip(users, items))
+        couples = [list(elem) for elem in couples]
+        data = self.__URM.data
+        couples_train, couples_test, data_train, data_test = train_test_split(
+            couples, data,
+            test_size=1 - percentage_of_training_data,
+            shuffle=True,
+            random_state=1234
+        )
+        users_training, users_testing, items_training, items_testing = self.__unzip_couples(couples_train, couples_test)
+        urm_training = sparse.coo_matrix((data_train, (users_training, items_training)), shape=(n_users, n_items))
+        urm_training = sparse.csr_matrix(urm_training)
+        urm_testing = sparse.coo_matrix((data_test, (users_testing, items_testing)), shape=(n_users, n_items))
+        urm_testing = sparse.csr_matrix(urm_testing)
+        return urm_training, urm_testing
+
+    def deterministic_hold_out_ratings_validation(self, percentage_of_training_data, percentage_of_validation_data,
+                                                  percentage_of_testing_data=None):
+        self.__assert_train_test_val_percentage(percentage_of_training_data, percentage_of_validation_data,
+                                                percentage_of_testing_data)
+        tmp = percentage_of_training_data
+        percentage_of_training_data = percentage_of_training_data + percentage_of_validation_data
+        urm_training, urm_testing = self.deterministic_hold_out_ratings_train_test(percentage_of_training_data)
+        percentage_of_validation_data = percentage_of_validation_data / tmp
+        users, items = urm_training.nonzero()
+        n_users, n_items = np.max(users) + 1, np.max(items) + 1
+        couples = list(zip(users, items))
+        couples = [list(elem) for elem in couples]
+        data = urm_training.data
+        couples_train, couples_validation, data_train, data_validation = train_test_split(
+            couples, data,
+            test_size=percentage_of_validation_data,
+            shuffle=True,
+            random_state=1234
+        )
+        users_training, users_validation, items_training, items_validation = self.__unzip_couples(couples_train, couples_validation)
+        urm_training = sparse.coo_matrix((data_train, (users_training, items_training)), shape=(n_users, n_items))
+        urm_training = sparse.csr_matrix(urm_training)
+        urm_validation = sparse.coo_matrix((data_validation, (users_validation, items_validation)), shape=(n_users, n_items))
+        urm_validation = sparse.csr_matrix(urm_validation)
+        return urm_training, urm_validation, urm_testing
+
+    def get_all_data(self):
         return self.__URM
